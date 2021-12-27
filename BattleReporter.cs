853bee14f2832f14928b5d985d06a097e9f026df
sebastian <sebastian.ferngren@gmail.com>
2021-12-27 12:24:22 +0100

    make battlereporter push a messagestate to show the battle message

--- BattleReporter.cs
+++ BattleReporter.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using Monomon.Battle;
+using Monomon.Input;
+using Monomon.State;
+using Monomon.Views.Scenes;
 using System;
 using System.Collections.Generic;
 
@@ -8,22 +12,32 @@ namespace Monomon
     internal class BattleReporter : IBattleReporter
     {
         private readonly SpriteBatch batch;
+        private readonly StateStack<double> _stack;
+        private SpriteFont _font;
+        private IINputHandler _input;
+        private GraphicsDevice _gd;
 
         public List<string> Messages { get; set; }
-        public BattleReporter(SpriteBatch batch)
+        public BattleReporter(SpriteBatch batch, GraphicsDevice gd, State.StateStack<double> stack,IINputHandler input, SpriteFont font)
         {
+            _font = font; 
+            _input = input;
+            _gd = gd;
             if (batch is null)
             {
                 throw new ArgumentNullException(nameof(batch));
             }
 
             this.batch = batch;
+            _stack = stack;
             Messages = new List<string>();
         }
 
         public void OnAttack(BattleMessage message)
         {
-            Messages.Add($"{message.attacker} attacked {message.receiver} for {message.damage} points of damage!");
+            var str = $"{message.attacker} attacked {message.receiver} for {message.damage} points of damage!";
+            var view = new MessageScene(_gd, str, _font);
+            _stack.Push(new TimeoutState(view,1000,_input, () => { _stack.Pop(); }),() => { _stack.Pop(); });
         }
     }
 }
