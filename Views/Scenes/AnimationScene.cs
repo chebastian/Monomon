3fd3d2a3ae8bf6aac12f214e3406e37c561ef976
sebastian <sebastian.ferngren@gmail.com>
2021-12-28 12:49:57 +0100

    add animation scene

---
+++ Views/Scenes/AnimationScene.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Monomon.Animation;
+using System;
+using System.Collections.Generic;
+
+namespace Monomon.Views.Scenes
+{
+    class AnimationScene : SceneView
+    {
+        private Vector2 _pos;
+        private Texture2D _tex;
+        private AnimationPlayer player;
+
+        public AnimationScene(GraphicsDevice gd, Vector2 pos, Texture2D tex, List<Frame> anim) : base(gd)
+        {
+            _pos = pos;
+            _tex = tex;
+            player = new AnimationPlayer(1);
+            player.ChangeAnimation(anim);
+        }
+
+        public override void LoadScene(ContentManager content)
+        {
+        }
+
+        public override void Update(double time)
+        {
+            player.Update((float)time);
+        }
+
+        protected override void OnDraw(SpriteBatch batch)
+        {
+            var frame = player.CurrentFrame();
+            batch.Draw(_tex, _pos,new Rectangle(frame.Source.x,frame.Source.y,24,24), Color.White);
+        }
+    }
+
+    class TestScene : SceneView
+    {
+        private readonly Action<double> update;
+        private readonly Action<SpriteBatch> render;
+
+        public TestScene(GraphicsDevice gd,Action<double> update, Action<SpriteBatch> render) : base(gd)
+        {
+            this.update = update;
+            this.render = render;
+        }
+
+        public override void LoadScene(ContentManager content)
+        {
+        }
+
+        public override void Update(double time)
+        {
+            update(time);
+        }
+
+        protected override void OnDraw(SpriteBatch batch)
+        {
+            render(batch);
+        }
+    }
+}
