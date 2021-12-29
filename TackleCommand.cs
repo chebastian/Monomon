13bac8403c241e4d068969e106bdc694bc5d8fff
sebastian <sebastian.ferngren@gmail.com>
2021-12-29 21:38:52 +0100

    add slash, swipe and tackle commands

--- TackleCommand.cs
+++ TackleCommand.cs
@@ -6,9 +6,13 @@ namespace Monomon.Battle
     { 
         Tackle,
         Slash,
+        Swipe,
         Wrap,
     } 
 
     public record MonStatus(int attack, int defense, int speed);
     public record AttackCommand(AttackType attackType, MonStatus stat);
+    public record Tackle(MonStatus stat) : AttackCommand(AttackType.Tackle,stat);
+    public record Slash(MonStatus stat) : AttackCommand(AttackType.Slash,stat with { attack = stat.attack + 2 });
+    public record Swipe(MonStatus stat) : AttackCommand(AttackType.Swipe,stat with { attack = stat.attack + 7 });
 }
