448e5d589c8b3bebd9163720522d253ca65b746b
sebastian <sebastian.ferngren@gmail.com>
2022-05-28 12:47:24 +0200

    make current palette a property on palette effect

--- Effects/PaletteEffect.cs
+++ Effects/PaletteEffect.cs
@@ -7,6 +7,8 @@ namespace Monomon.Effects
     {
         private Effect paletteEffect;
         private Texture2D _palette;
+        private float currentPalette;
+
         public PaletteEffect(ContentManager content, Texture2D paletteTexture)
         {
             paletteEffect = content.Load<Effect>("Indexed");
@@ -15,6 +17,15 @@ namespace Monomon.Effects
             paletteEffect.Parameters["time"].SetValue(0.0f);
             paletteEffect.Parameters["swap"].SetValue(1.0f);
             paletteEffect.Parameters["palette"].SetValue(_palette);
+        }
+
+        public float CurrentPalette
+        {
+            get => currentPalette; set
+            {
+                currentPalette = value;
+                paletteEffect.Parameters["time"].SetValue(value);
+            }
         }
 
         public void EffectBegin(SpriteBatch batch)
