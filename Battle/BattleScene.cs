4003c0d264893b385539779cfead8297aeebb7ae
sebastian <sebastian.ferngren@gmail.com>
2022-05-28 10:26:56 +0200

    add battlescene

---
+++ Battle/BattleScene.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Monomon.Views.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monomon.Battle;
+
+public class BattleScene : SceneView
+{
+    public override void LoadScene(ContentManager content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update(double time)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OnDraw(SpriteBatch batch)
+    {
+        throw new NotImplementedException();
+    }
+}
