988b3a9f2fd4f6ad3e52ee412957636b1e12959f
sebastian <sebastian.ferngren@gmail.com>
2022-05-28 13:02:54 +0200

    make swipe a killer move

--- TackleCommand.cs
+++ TackleCommand.cs
@@ -13,6 +13,6 @@ namespace Monomon.Battle
     public record MonStatus(int attack, int defense, int speed);
     public record AttackCommand(AttackType attackType, MonStatus stat);
     public record Tackle(MonStatus stat) : AttackCommand(AttackType.Tackle,stat);
-    public record Slash(MonStatus stat) : AttackCommand(AttackType.Slash,stat with { attack = stat.attack + 2 });
+    public record Slash(MonStatus stat) : AttackCommand(AttackType.Slash,stat with { attack = stat.attack + 12 });
     public record Swipe(MonStatus stat) : AttackCommand(AttackType.Swipe,stat with { attack = stat.attack + 7 });
 }
