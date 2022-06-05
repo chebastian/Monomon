dd16bbf149b850614a883fa77326a36021f7f56c
sebastian <sebastian.ferngren@gmail.com>
2022-06-05 12:56:12 +0200

    refactor, samplescene filescoped namespace

--- Views/Samples/SampleScene.cs
+++ Views/Samples/SampleScene.cs
@@ -15,101 +15,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Monomon.Views.Samples
+namespace Monomon.Views.Samples;
+
+public class SampleScene : SceneView
 {
-    public class SampleScene : SceneView
+    private SpriteFont font;
+    private UIList<string> _sceneList;
+    private IINputHandler _input;
+    private StateStack<double> _stack;
+
+    public GraphicsDevice GraphicsDevice { get; }
+
+    public SampleScene(GraphicsDevice gd, StateStack<double> stack, IINputHandler input, ContentManager content, PaletteEffect effect, FadeEffect fade)
+        : base(gd,content)
+
     {
-        private SpriteFont font;
-        private UIList<string> _sceneList;
-        private IINputHandler _input;
-        private StateStack<double> _stack;
+        _input = input;
+        _stack = stack;
+        GraphicsDevice = gd;
+        font = content.Load<SpriteFont>("File");
+        var sprites = content.Load<Texture2D>("spritemap");
 
-        public GraphicsDevice GraphicsDevice { get; }
 
-        public SampleScene(GraphicsDevice gd, StateStack<double> stack, IINputHandler input, ContentManager content, PaletteEffect effect, FadeEffect fade)
-            : base(gd,content)
 
+        _sceneList = new UIList<string>(new List<UIItem<string>>() {
+            new UIItem<string>("Level test",x => {
+                SwapScene(new LevelSample(GraphicsDevice,_input,_stack,content,effect,fade));
+            }),
+            new UIItem<string>("Battle test",x => {
+                //SwapScene(new BattleSample(GraphicsDevice,_input,_stack,_content,effect,fade));
+            }),
+            new UIItem<string>("Tween",x => {
+                SwapScene(new TweenSamples(GraphicsDevice,_content));
+            }),
+            new UIItem<string>("Message",x => {
+                SwapScene(new MessageScene(GraphicsDevice,"First message",font,sprites,_content));
+            }),
+            new UIItem<string>("Long Message",x => {
+                SwapScene(new MessageScene(GraphicsDevice,"First message aaa aa a aaa a aa aaaa aaa aaaa a aa aaa a aaaaaaa aaaaaaaaaaaaa aaa aaaaaaaaaaaaaaaaaaaaa aaa",font,sprites,_content));
+            }),
+            new UIItem<string>("Long single worded message",x => {
+                SwapScene(new MessageScene(GraphicsDevice,"first message aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",font,sprites,_content));
+            }),
+            new UIItem<string>("Timed message 2",x => {
+
+                var scene =new MessageScene(GraphicsDevice,"Completes in for 2s...",font,sprites,_content);
+                scene.LoadScene(content);
+                _stack.Push(new TimeoutState(scene,2000,_input,
+                onCancel: () => {
+                    _stack.Pop();
+                }),
+                onCompleted: () => 
+                {
+                    _stack.Pop();
+                    SwapScene(new MessageScene(GraphicsDevice,"Completed!",font,sprites,_content)); }
+                );
+        ;
+            }),
+            new UIItem<string>("BattleCard sample",x => {
+                SwapScene(new BattleCardSample(GraphicsDevice,_content));
+            }),
+            new UIItem<string>("Empty",x => {
+                SwapScene(new EmptyScene(GraphicsDevice,_content));
+            }),
+        }, x => { }, x => { });
+
+    }
+
+    public override void LoadScene(ContentManager content)
+    {
+        _content = content;
+        //SwapScene(new LevelSample(GraphicsDevice,_input,_stack,content));
+    }
+
+    private void SwapScene(SceneView newScene)
+    {
+        _stack.Push(new SceneState(newScene, _input), () =>
         {
-            _input = input;
-            _stack = stack;
-            GraphicsDevice = gd;
-            font = content.Load<SpriteFont>("File");
-            var sprites = content.Load<Texture2D>("spritemap");
-    
+            _stack.Pop();
+        },() => { });
+        newScene.LoadScene(_content);
+    }
 
+    public override void Update(double time)
+    {
+        if (_input.IsKeyPressed(KeyName.Down))
+            _sceneList.SelectNext();
+        if (_input.IsKeyPressed(KeyName.Up))
+            _sceneList.SelectPrevious();
+        if (_input.IsKeyPressed(KeyName.Select))
+            _sceneList.Select();
+    }
 
-            _sceneList = new UIList<string>(new List<UIItem<string>>() {
-                new UIItem<string>("Level test",x => {
-                    SwapScene(new LevelSample(GraphicsDevice,_input,_stack,content,effect,fade));
-                }),
-                new UIItem<string>("Battle test",x => {
-                    //SwapScene(new BattleSample(GraphicsDevice,_input,_stack,_content,effect,fade));
-                }),
-                new UIItem<string>("Tween",x => {
-                    SwapScene(new TweenSamples(GraphicsDevice,_content));
-                }),
-                new UIItem<string>("Message",x => {
-                    SwapScene(new MessageScene(GraphicsDevice,"First message",font,sprites,_content));
-                }),
-                new UIItem<string>("Long Message",x => {
-                    SwapScene(new MessageScene(GraphicsDevice,"First message aaa aa a aaa a aa aaaa aaa aaaa a aa aaa a aaaaaaa aaaaaaaaaaaaa aaa aaaaaaaaaaaaaaaaaaaaa aaa",font,sprites,_content));
-                }),
-                new UIItem<string>("Long single worded message",x => {
-                    SwapScene(new MessageScene(GraphicsDevice,"first message aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",font,sprites,_content));
-                }),
-                new UIItem<string>("Timed message 2",x => {
-
-                    var scene =new MessageScene(GraphicsDevice,"Completes in for 2s...",font,sprites,_content);
-                    scene.LoadScene(content);
-                    _stack.Push(new TimeoutState(scene,2000,_input,
-                    onCancel: () => {
-                        _stack.Pop();
-                    }),
-                    onCompleted: () => 
-                    {
-                        _stack.Pop();
-                        SwapScene(new MessageScene(GraphicsDevice,"Completed!",font,sprites,_content)); }
-                    );
-            ;
-                }),
-                new UIItem<string>("BattleCard sample",x => {
-                    SwapScene(new BattleCardSample(GraphicsDevice,_content));
-                }),
-                new UIItem<string>("Empty",x => {
-                    SwapScene(new EmptyScene(GraphicsDevice,_content));
-                }),
-            }, x => { }, x => { });
-
-        }
-
-        public override void LoadScene(ContentManager content)
-        {
-            _content = content;
-            //SwapScene(new LevelSample(GraphicsDevice,_input,_stack,content));
-        }
-
-        private void SwapScene(SceneView newScene)
-        {
-            _stack.Push(new SceneState(newScene, _input), () =>
-            {
-                _stack.Pop();
-            },() => { });
-            newScene.LoadScene(_content);
-        }
-
-        public override void Update(double time)
-        {
-            if (_input.IsKeyPressed(KeyName.Down))
-                _sceneList.SelectNext();
-            if (_input.IsKeyPressed(KeyName.Up))
-                _sceneList.SelectPrevious();
-            if (_input.IsKeyPressed(KeyName.Select))
-                _sceneList.Select();
-        }
-
-        protected override void OnDraw(SpriteBatch batch)
-        {
-            ListView.DrawUIList(_sceneList, new Vector2(10, 10),batch,font);
-        }
+    protected override void OnDraw(SpriteBatch batch)
+    {
+        ListView.DrawUIList(_sceneList, new Vector2(10, 10),batch,font);
     }
 }
