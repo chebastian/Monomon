6e66f2e5e82c90eb6e4b24e8f5bef5dfa9060e11
sebastian <sebastian.ferngren@gmail.com>
2022-10-17 07:15:27 +0200

    bugfix, fit entire string before rendering
    this fixes issue with jumping words

--- Views/Scenes/MessageScene.cs
+++ Views/Scenes/MessageScene.cs
@@ -14,23 +14,29 @@ namespace Monomon.Views.Scenes
     class MessageScene : SceneView
     {
         private readonly bool _confirm;
-        private readonly string message;
+        private readonly string _message;
+        private readonly float _panelH;
         private SpriteFont _font;
         private Texture2D _sprites;
         private int _charCount;
         private float _totalTime;
+        private int _padding;
 
         public MessageScene(GraphicsDevice gd, string message, SpriteFont font, Texture2D sprites, ContentManager mgr, bool confirm = false) : base(gd, mgr)
         {
             _confirm = confirm;
-            this.message = message;
+            _message = message;
             if (confirm)
-                this.message += " >>";
+                _message += " >>";
             _font = font;
             _sprites = sprites;
 
             _charCount = 0;
             _totalTime = 0.0f;
+
+            _padding = 8;
+            _message = FitString(_message, message.Length, panelw - _padding * 2, _font);
+            _panelH = Math.Max(_font.MeasureString(_message).Y,30);
         }
 
         public override void LoadScene(ContentManager content)
@@ -40,7 +46,7 @@ namespace Monomon.Views.Scenes
         public override void Update(double time)
         {
             _totalTime += (float)time;
-            _charCount = (int)(Math.Min(1.0f, _totalTime) * message.Length);
+            _charCount = (int)(Math.Min(1.0f, _totalTime) * _message.Length);
         }
 
         public bool IsCompleted => _totalTime >= 1.0f;
@@ -53,7 +59,7 @@ namespace Monomon.Views.Scenes
             var completeString = "";
             while (words.Length > wordsAdded)
             {
-                var nextString = $"{completeString} {words[wordsAdded]}";
+                var nextString = $"{completeString} {words[wordsAdded]}".TrimStart();
                 if (font.MeasureString(nextString).X > maxWidth)
                 {
                     completeString = $"{completeString} \n {words[wordsAdded]}";
@@ -76,22 +82,18 @@ namespace Monomon.Views.Scenes
             var meassured = false;
             if (meassured)
             {
-                var sz = _font.MeasureString(message);
+                var sz = _font.MeasureString(_message);
                 Panel.Draw(batch, _sprites, Panel.BasePanel, new Rectangle(100, UIValues.BattleMessageY, (int)(sz.X + 40), (int)(sz.Y + 20)));
-                batch.DrawString(_font, message, new Vector2(120, UIValues.BattleMessageY + 10), Color.White);
+                batch.DrawString(_font, _message, new Vector2(120, UIValues.BattleMessageY + 10), Color.White);
             }
             else
             {
                 int panelx = 0;
-                int padding = 8;
-                var renderString = FitString(message, _charCount, panelw - (padding * 2), _font);
-                var height = Math.Min(_font.MeasureString(renderString).Y, 100);
-                Panel.Draw(batch, _sprites, Panel.BasePanel, new Rectangle(panelx, UIValues.BattleMessageY, panelw, (int)(height + padding)));
+                var renderString = _message.Substring(0, _charCount);
+                Panel.Draw(batch, _sprites, Panel.BasePanel, new Rectangle(panelx, UIValues.BattleMessageY, panelw, (int)(_panelH + _padding)));
 
 
-                batch.DrawString(_font, renderString, new Vector2(panelx + padding, UIValues.BattleMessageY + padding), Color.White);
-                //if (_confirm)
-                //    batch.DrawString(_font, "X", new Vector2(panelx + panelw - padding - padding, UIValues.BattleMessageY + height - padding), Color.Red);
+                batch.DrawString(_font, renderString, new Vector2(panelx + _padding, UIValues.BattleMessageY + _padding), Color.White);
             }
         }
     }
